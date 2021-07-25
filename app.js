066f6a52c82837a7599eea98fdc98c3e20fc06f4
Bassel07 <basel.rizeq93@gmail.com>
2021-07-25 04:28:09 -0700

    addedfiles

--- app.js
+++ app.js
@@ -1,4 +1,7 @@
 'use strict'
+
+let booksnames = ['1','2','3','4','5'] 
+
 let container = document.getElementById('container');
 let tableEl = document.createElement('table');
 container.appendChild(tableEl);
@@ -13,5 +16,11 @@ this.thePrice = thePrice;
 books.push(this)
 }
 
-
+book.prototype.calcTotal = function calcTotal(min, max) {
+            min = Math.ceil(min);
+            max = Math.floor(max);
+            return Math.floor(Math.random() * (max - min + 1)) + min; 
+          }
+    }
+}
 
